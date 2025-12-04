5885ae48c4d173b9c175099df72938a63252c9f3
skilljava <claimyournitrobussiness@gmail.com>
2025-12-04 22:47:53 +0330

    Create invoices.php

---
+++ invoices.php
@@ -0,0 +1,14 @@
+<?php
+require 'header.php';
+?>
+<div class="container">
+    <h2>فاکتورها</h2>
+    <table class="table">
+        <tr><th>شماره فاکتور</th><th>تاریخ</th><th>مبلغ</th><th>وضعیت</th></tr>
+        <tr><td>INV001</td><td>2025/11/01</td><td>100,000 تومان</td><td>پرداخت شده</td></tr>
+        <tr><td>INV002</td><td>2025/11/20</td><td>200,000 تومان</td><td>پرداخت نشده</td></tr>
+    </table>
+</div>
+<?php
+require 'footer.php';
+?>
