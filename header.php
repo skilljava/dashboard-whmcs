d367c84ec1fe9b0fc37461a0aaef13dc84fd881e
skilljava <claimyournitrobussiness@gmail.com>
2025-12-04 22:48:33 +0330

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AriaHost - داشبورد</title>
+    <title>داشبورد</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -12,10 +12,11 @@
         <h1>AriaHost Dashboard</h1>
         <nav class="top-nav">
             <ul>
-                <li><a href="#">داشبورد</a></li>
-                <li><a href="#">سرویس‌ها</a></li>
-                <li><a href="#">فاکتورها</a></li>
-                <li><a href="#">پشتیبانی</a></li>
+                <li><a href="index.php">داشبورد</a></li>
+                <li><a href="services.php">سرویس‌ها</a></li>
+                <li><a href="invoices.php">فاکتورها</a></li>
+                <li><a href="tickets.php">پشتیبانی</a></li>
+                <li><a href="account.php">حساب کاربری</a></li>
             </ul>
         </nav>
     </div>
