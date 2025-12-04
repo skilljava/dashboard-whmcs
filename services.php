2cb876eb149a32fb02c6ae0cde9eb6de3e2941f3
skilljava <claimyournitrobussiness@gmail.com>
2025-12-04 22:47:33 +0330

    Create services.php

---
+++ services.php
@@ -0,0 +1,14 @@
+<?php
+require 'header.php';
+?>
+<div class="container">
+    <h2>سرویس‌ها</h2>
+    <table class="table">
+        <tr><th>شناسه</th><th>نام سرویس</th><th>وضعیت</th><th>تاریخ پایان</th></tr>
+        <tr><td>SV001</td><td>هاست لینوکس S</td><td>Active</td><td>2026/01/10</td></tr>
+        <tr><td>SV002</td><td>VPS 2GB</td><td>Suspended</td><td>2025/12/01</td></tr>
+    </table>
+</div>
+<?php
+require 'footer.php';
+?>
