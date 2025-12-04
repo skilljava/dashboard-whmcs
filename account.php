b09f8e13e0d10c07aa37a936b37af788e52bc5b0
skilljava <claimyournitrobussiness@gmail.com>
2025-12-04 22:48:16 +0330

    Create account.php

---
+++ account.php
@@ -0,0 +1,14 @@
+<?php
+require 'header.php';
+?>
+<div class="container">
+    <h2>مشخصات حساب کاربری</h2>
+    <table class="table">
+        <tr><th>نام</th><td>آراد برهان</td></tr>
+        <tr><th>ایمیل</th><td>arad@example.com</td></tr>
+        <tr><th>شماره تلفن</th><td>09123456789</td></tr>
+    </table>
+</div>
+<?php
+require 'footer.php';
+?>
