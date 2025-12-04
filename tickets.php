e8abd5859230955f2c77027c0a770e7206488bbf
skilljava <claimyournitrobussiness@gmail.com>
2025-12-04 22:48:06 +0330

    Create tickets.php

---
+++ tickets.php
@@ -0,0 +1,14 @@
+<?php
+require 'header.php';
+?>
+<div class="container">
+    <h2>تیکت‌ها / پشتیبانی</h2>
+    <table class="table">
+        <tr><th>شناسه</th><th>موضوع</th><th>وضعیت</th><th>آخرین پاسخ</th></tr>
+        <tr><td>TCK001</td><td>مشکل ورود به FTP</td><td>باز</td><td>2025/12/01</td></tr>
+        <tr><td>TCK002</td><td>تمدید دامنه</td><td>بسته</td><td>2025/11/25</td></tr>
+    </table>
+</div>
+<?php
+require 'footer.php';
+?>
